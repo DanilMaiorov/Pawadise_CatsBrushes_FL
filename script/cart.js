ca352a36df721c9f7efb28f3ce3e5c7d4b48d037
Danil Maiorov <damayorov93@gmail.com>
2022-08-27 22:11:24 +0300

    add cart

---
+++ script/cart.js
@@ -0,0 +1,152 @@
+/*CART*/
+const cartBtn = document.querySelector('.header__nav-shoppingbag');
+const burgerCartBtn = document.querySelector('.header__shoppingbag');
+const openCartArr = [cartBtn, burgerCartBtn];
+const cartModal = document.querySelector('.cart');
+const cartClose = document.querySelector('.cart__close');
+const addToCartBtn = document.querySelector('.green-btn');
+const cartWrapper = document.querySelector('.cart__wrapper');
+const sumCart = document.querySelector('.cart__total > span');
+const cartArr = localStorage.getItem('cart') ?
+    JSON.parse(localStorage.getItem('cart')) : [];
+
+
+
+const renderCart = (goods) => {
+    cartWrapper.innerHTML = '';
+    if (goods.length === 0) {
+        cartWrapper.innerHTML =
+            `<div class="cart__empty">
+                Your cart is empty yet
+            </div>`;
+    } else {
+        goods.forEach((good, index) => {
+            const newCard = document.createElement('div');
+            newCard.classList.add('card');
+            //data-key="${goodsItem.id}
+            newCard.innerHTML = `
+            <div class="card__img-wrapper">
+                <img src="${good.image}" alt="">
+            </div>
+            <div class="card__body">
+                <div class="card__price">$ ${good.price}</div>
+                <h5 class="card__title">${good.title}</h5>
+                <div class="card__descr">
+                    <button class="card__btn-minus">-</button>
+                    <div class="card__count">${good.count}</div>
+                    <button class="card__btn-plus">+</button>
+                    <div class="card__total" data-sum="${index}"> $${+good.price * +good.count}</div>
+                </div>
+                <button type="button" class="btn green-btn delete-btn">Delete</button>
+            </div>
+        </div>`;
+            cartWrapper.append(newCard);
+            newCard.addEventListener('click', (e) => {
+                if (e.target.classList.contains('card__btn-minus')) {
+                    minusCartItem(good.id);
+                } else if (e.target.classList.contains('card__btn-plus')) {
+                    plusCartItem(good.id);
+                } else if (e.target.classList.contains('delete-btn')) {
+                    deleteCartItem(good.id);
+                }
+            });
+        });
+    }
+};
+
+function addToCart(id) { //функция добавления товаров в корзину
+    const goods = JSON.parse(localStorage.getItem('goods'));
+    const clickedGood = goods.find((good) => {
+        return good.id === +id;
+    });
+    const cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+    if (cart.some(good => good.id === clickedGood.id)) {
+        cart.map(good => {
+            if (good.id === clickedGood.id) {
+                good.count++;
+            }
+            return good;
+        });
+    } else {
+        clickedGood.count = 1;
+        cart.push(clickedGood);
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function deleteCartItem(id) { //функция удаления элементов из корзины
+    let cart = JSON.parse(localStorage.getItem('cart'));
+    const newCart = cart.filter(good => {
+        return good.id !== id;
+    });
+    localStorage.setItem('cart', JSON.stringify(newCart));
+    if (cart = []) {
+        sumCart.innerHTML = 0;
+    }
+    actuallyCart();
+}
+
+function plusCartItem(id) { //функция удаления элементов из корзины
+    const cart = JSON.parse(localStorage.getItem('cart'));
+    const newCart = cart.map(good => {
+        if (good.id === id) {
+            good.count++;
+        }
+        return good;
+    });
+    localStorage.setItem('cart', JSON.stringify(newCart));
+    actuallyCart();
+}
+
+function minusCartItem(id) { //функция удаления элементов из корзины
+    const cart = JSON.parse(localStorage.getItem('cart'));
+    const newCart = cart.map(good => {
+        if (good.id === id) {
+            if (good.count > 0) {
+                good.count--;
+            }
+        }
+        return good;
+    });
+    localStorage.setItem('cart', JSON.stringify(newCart));
+    actuallyCart();
+}
+
+function actuallyCart() { //функция для отсутствия дублирования кода
+    renderCart(JSON.parse(localStorage.getItem('cart')));
+    console.log();
+    summary(JSON.parse(localStorage.getItem('cart')));
+}
+
+function summary(goods) { //функция сложения
+    let sum = 0;
+    goods.forEach(good => {
+        let summ = +good.price * +good.count;
+        sum += summ;
+        sumCart.innerHTML = `${sum.toFixed(2)}`;
+    });
+}
+const openCart = () => {
+    const cart = localStorage.getItem('cart') ?
+        JSON.parse(localStorage.getItem('cart')) : [];
+    cartModal.style.display = 'block';
+    sumCart.innerHTML = '0';
+    summary(JSON.parse(localStorage.getItem('cart')));
+    renderCart(cart);
+};
+console.log(cartModal);
+addToCartBtn.addEventListener('click', () => {
+    icons.forEach(item => {
+        if (item.closest('.active')) {
+            const key = item.dataset.color;
+            addToCart(key);
+        }
+    });
+});
+
+openCartArr.forEach(item => {
+    item.addEventListener('click', openCart);
+});
+cartClose.addEventListener('click', () => {
+    cartModal.style.display = 'none';
+});
--- script/tabs.js
+++ script/tabs.js
@@ -1,77 +1,144 @@
+/* TABS */
 
-/* SLIDER */
+const largeImg = document.querySelector('.images-part__lagre');
+const gallery = Array.from(document.querySelectorAll('.item-img'));
+const tabsImage = document.querySelector('.images-part__lagre-img');
+const iconsPanel = document.querySelector('.part-colors__items');
+const icons = document.querySelectorAll('.color-item');
+const iconsContent = document.querySelectorAll('.part__mini-items');
+const buttonsColor = document.querySelectorAll('.colors-button'); 
 
-const largeImg = document.querySelector('.images-part__lagre')
-const gallery = Array.from(document.querySelectorAll('.item-img'))
-const tabsImage = document.querySelector('.images-part__lagre-img')
-const iconsPanel = document.querySelector('.part-colors__items')
-const icons = document.querySelectorAll('.color-item')
-const iconsContent = document.querySelectorAll('.part__mini-items')
 
-const tabsImages = [ { image: 'img/product-1.png' },{ image: 'img/product-2-large.png' }, { image: 'img/product-3-large.png' }, 
-    { image: 'img/product-4-large.png' }, { image: 'img/product-5-large.png' }, { image: 'img/product-2-large.png'}, 
-    { image: 'img/product-1.png' }, { image: 'img/product-4-large.png' }, { image: 'img/product-3-large.png' }, 
-    { image: 'img/product-5-large.png' }, { image: 'img/product-3-large.png' }, { image: 'img/product-2-large.png' }, 
-    { image: 'img/product-1.png' }, { image: 'img/product-5-large.png' }, { image: 'img/product-4-large.png' },
-] 
-const largeImages = [ { image: 'img/product-1.png' },{ image: 'img/product-2-large.png' }, { image: 'img/product-3-large.png' },
-]
+const tabsImages = [{
+        image: 'img/product-1.png'
+    },
+    {
+        image: 'img/product-2-large.png'
+    },
+    {
+        image: 'img/product-3-large.png'
+    },
+    {
+        image: 'img/product-4-large.png'
+    },
+    {
+        image: 'img/product-5-large.png'
+    },
+    {
+        image: 'img/product-2-large.png'
+    },
+    {
+        image: 'img/product-1.png'
+    },
+    {
+        image: 'img/product-3-large.png'
+    },
+    {
+        image: 'img/product-4-large.png'
+    },
+    {
+        image: 'img/product-5-large.png'
+    },
+    {
+        image: 'img/product-3-large.png'
+    },
+    {
+        image: 'img/product-2-large.png'
+    },
+    {
+        image: 'img/product-1.png'
+    },
+    {
+        image: 'img/product-5-large.png'
+    },
+    {
+        image: 'img/product-4-large.png'
+    },
+];
+const itemCard = [{
+        id: 0,
+        image: 'img/product-1.png',
+        color: 'green',
+        price: 29.99,
+        sale: true,
+        title: 'Green Grooming Brush',
+    },
+    {
+        id: 1,
+        image: 'img/product-2-large.png',
+        color: 'pink',
+        price: 29.99,
+        sale: true,
+        title: 'Pink Grooming Brush',
+    },
+    {
+        id: 2,
+        image: 'img/product-3-large.png',
+        color: 'white',
+        price: 29.99,
+        sale: true,
+        title: 'White Grooming Brush',
+    },
+];
+localStorage.setItem('goods', JSON.stringify(itemCard));
 
 const changeLarge = (index) => {
-    tabsImage.setAttribute('src', largeImages[index].image)
-}
+    tabsImage.setAttribute('src', itemCard[index].image);
+};
 const changeContent = (index) => {
-        tabsImage.setAttribute('src', tabsImages[index].image)
-}
+    tabsImage.setAttribute('src', tabsImages[index].image);
+};
 const changeActiveTabs = (indexClickedTab) => {
     gallery.forEach((tab, index) => {
-        tab.classList.remove('active')
-        if(index === indexClickedTab) {
-            tab.classList.add('active')
+        tab.classList.remove('active');
+        if (index === indexClickedTab) {
+            tab.classList.add('active');
         }
-    })
-    changeContent(indexClickedTab)
-}
-
+    });
+    changeContent(indexClickedTab);
+};
 const changeActiveIcon = (indexClickedIcon) => {
     icons.forEach((tab, index) => {
-        tab.classList.remove('active')
-        if(index === indexClickedIcon) {
-            tab.classList.add('active')
+        tab.classList.remove('active');
+        if (index === indexClickedIcon) {
+            tab.classList.add('active');
         }
-    })
-    changeLarge (indexClickedIcon)
-}
-
+    });
+    changeLarge(indexClickedIcon);
+};
 iconsPanel.addEventListener('click', (e) => {
-    if(e.target.closest('.color-item')) {
-        const iconBtn = e.target.closest('.color-item')
+    if (e.target.closest('.color-item')) {
+        const iconBtn = e.target.closest('.color-item');
         icons.forEach((icon, index) => {
-            if(icon === iconBtn) {
-                icon.classList.add('active')
-                iconsContent[index].classList.remove('hidden')
+            if (icon === iconBtn) {
+                icon.classList.add('active');
+                iconsContent[index].classList.remove('hidden');
             } else {
-                icon.classList.remove('active')
-                iconsContent[index].classList.add('hidden')
+                icon.classList.remove('active');
+                iconsContent[index].classList.add('hidden');
             }
-        })
+        });
     }
-})
-
+});
 icons.forEach((icon, index) => {
     icon.addEventListener('click', () => {
-        (index < 1) ? changeActiveTabs(0) : (index === 1) ? changeActiveTabs(5) : (index > 1) ? changeActiveTabs(10) : changeActiveTabs(0)
-        changeActiveIcon(index)
-    })
-    
-})
-
+        (index < 1) ? changeActiveTabs(index): (index === 1) ? changeActiveTabs(iconsContent[index].children.length) : (index > 1) ? changeActiveTabs(iconsContent[index].children.length * index) : changeActiveTabs(index);
+        changeActiveIcon(index);
+    });
+});
 gallery.forEach((tab, index) => {
     tab.addEventListener('click', () => {
-        changeActiveTabs(index)
-    })
-})
+        changeActiveTabs(index);
+    });
+});
 
-
-
+buttonsColor.forEach((item, index) => {
+    item.addEventListener('click', (e) => {
+        if(e.target === item) {
+            changeActiveIcon(index);
+            changeActiveTabs(index);
+        }
+    });
+    
+});
 
